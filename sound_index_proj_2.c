a517299f9732c7e07d63f1c0a4e791741d48f3d4
AlexLassooij <71570400+AlexLassooij@users.noreply.github.com>
2021-04-08 12:44:03 -0700

    Add files via upload

---
+++ sound_index_proj_2.c
@@ -0,0 +1,22 @@
+// Approximate index of sounds in file 'proj_2_speech_1.wav'
+code const unsigned long int wav_index[]={
+    0x0033f8, // 0 
+    0x00789e, // 1 
+    0x00c038, // 2 
+    0x00fe01, // 3 
+    0x013efd, // 4 
+    0x016ebc, // 5 
+    0x0187ce, // 6 
+    0x01d928, // 7 
+    0x021c6f, // 8 
+    0x026cca, // 9 
+    0x02ab30, // 10 
+    0x02f367, // 11 
+    0x0329cf, // 12 
+    0x03580f, // 13 
+    0x038fe6, // 14 
+    0x03d39b, // 15 
+    0x04159b, // 16 
+    0x0454e6, // 17 
+    0x049468
+};
